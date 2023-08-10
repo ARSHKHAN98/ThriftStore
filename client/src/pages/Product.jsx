e6e06130713d2371551eb1943310f7427c2fe3c7
Mohammed Arsh Khan <20bcs138@ietdavv.edu.in>
2023-08-10 14:08:46 +0530

    first commit

---
+++ client/src/pages/Product.jsx
@@ -0,0 +1,174 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import { mobile } from "../responsive";
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { publicRequest } from "../requestMethod";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  ${mobile({ padding: "10px", flexDirection: "column" })}
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  max-height: 700px;
+  max-width: 500px;
+  object-fit: contain;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+  ${mobile({ padding: "10px" })}
+`;
+const Brand = styled.h1`
+  font-weight: 400;
+  font-size: 40px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+  font-size: 25px;
+  color: gray;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+  font-size: 18px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 30px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })}
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 400;
+`;
+
+const FilterColor = styled.div`
+  margin-left: 5px;
+  padding: 5px;
+  color: gray;
+`;
+
+const FilterSize = styled.div`
+  margin-left: 5px;
+  padding: 5px;
+  color: gray;
+`;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ width: "100%" })}
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await publicRequest.get("/products/find/" + id);
+        setProduct(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getProduct();
+  }, []);
+
+  const handleClick = async () => {
+    try {
+      const userid = user._id;
+      const productid = product._id;
+      const check = userid + productid;
+      await publicRequest.post("/cart", {
+        userid,
+        productid,
+        check,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src={product.img} />
+        </ImgContainer>
+        <InfoContainer>
+          <Brand>{product.brand}</Brand>
+          <Title>{product.title}</Title>
+
+          <Desc>{product.desc}</Desc>
+          <Price>₹{product.price}</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor>{product.color}</FilterColor>
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize> {product.size}</FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <Button onClick={handleClick}>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
